be8329ada4b092b08e5511d4e77278b2da3db278
julianMGCODE <julianmosquerag@gmail.com>
2016-05-19 10:01:04 +0200

    Metodos terminados

--- src/my_facebook/Main_methods.java
+++ src/my_facebook/Main_methods.java
@@ -22,23 +22,27 @@ public class Main_methods {
     public void Post() throws FacebookException{    
    face.postStatusMessage("Hola");}
     
-    public void Postimg() throws MalformedURLException{
-        PostUpdate post = .picture(new URL())
+   /*
+    Para postear una imagen 
+    */
+    
+    public void Postimg() throws MalformedURLException, FacebookException{
+     face.postLink(new URL("https://www.facebook.com/julian.mosquera.31/posts/864374793666574"));
     }
     
     /*
     Le damos a me gusta a un post con tal de añadir la ID
     */
     
-    public void like(){
-        face.likePost(10153464860896846);
+    public void like() throws FacebookException{
+        face.likePost("864374793666574");
     }
     /*
-    
+    Comenta una un post
     */
     
-    public void Comentar(){
-        face.commentPhoto(id, "LOCO");
+   public void Comentar() throws FacebookException{
+        face.commentPhoto("851466124957441","probando cliente");
     }
 
     /*
@@ -48,8 +52,11 @@ public class Main_methods {
         ResponseList<Post> feed = face.getHome();
     }
 
+    /*
+    Buscamos un usuario
+    */
     public void tema() throws FacebookException{
-        ResponseList<Post> results = face.searchPosts("LOL");
+        ResponseList<User> results = face.searchUsers("Rubn Srz");
     }
 
 }
